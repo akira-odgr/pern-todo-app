2343c393860c6995bf911ba1459dac89a90b69f5
akira-odgr <177723350+akira-odgr@users.noreply.github.com>
2025-12-03 21:41:02 +0900

    preparation for deployment

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import todoRoutes from "./routes/todos.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.use("/todos", todoRoutes);
+
+app.listen(PORT, () => {
+    console.log("Server is listening port 5001");
+});
